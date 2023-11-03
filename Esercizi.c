581dd9e914860378186d9dd651cf35ab7af72b3c
OsvaldoRoscani <roscaniuni1@gmail.com>
2023-11-03 15:16:46 +0100

    uninettuno esercitazioni

---
+++ Esercizi.c
@@ -0,0 +1,20 @@
+/* in questo esercizio vediamo la logica dietro il puntatore classico in c in c++ e la dove utiliziamo i puntatori*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main (){
+
+int x = 5;
+int *px;
+
+
+px = &x;
+
+printf("x = %d (opper &d) \n, x, *px")
+printf()
+
+
+}
