bf4306ca58c42c485cf629c7acb039d25df36e4f
OsvaldoRoscani <roscaniuni1@gmail.com>
2023-11-07 15:49:48 +0100

    f

--- lezione 2.c
+++ lezione 2.c
@@ -14,4 +14,18 @@ bloblemi di uso errato
 
 
 //allocazione statica di memoria e dinamica di memoria 
+/*
+l'allocazione statica non ti consente di modificare una varibile decisa,
+
+
+l'allocazione dinamica della memoria ci permette di manipolare la memoria e efficentare la memoria perchè 
+porta ad utilizzare al meglio poco spazio di memoria e non sovraccaricare 
+il sistema.
+
+ */
+
+
+// esempio: 
+
+
 
